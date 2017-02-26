58e2d8d553f7b1ee63f035b0fea592ec9e9f4a86
benitron <benitron.bw@gmail.com>
2017-02-25 18:13:02 -0700

    sync

---
+++ app/src/main/java/ben/wheeler/studycards/Card.java
@@ -0,0 +1,14 @@
+package ben.wheeler.studycards;
+import android.text.*;
+import android.view.*;
+
+public class Card
+{
+	private String front, back;
+	private int correct, wrong;
+	
+	public Card(String file)
+	{
+		
+	}
+}
